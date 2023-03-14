e6d772c7bd9444f2f21bfffcebf45ce9ca043845
Duong Giang <giangdt1@vmoev.com>
2023-03-14 16:29:35 +0700

    aaaaa

--- js/wv.js
+++ js/wv.js
@@ -17,3 +17,22 @@ function check() {
 }
 
 
+function check1() {
+    if (
+        document.forms[0].elements[0].checked == true &&
+        document.forms[0].elements[1].checked == true &&
+        document.forms[0].elements[2].checked == true
+    ) {
+        if (!$(".wrapper").hasClass("throb")) {
+            // Do things on Nav Close
+            $(".wrapper").addClass("throb");
+        }
+    } else {
+        if ($(".wrapper").hasClass("throb")) {
+            // Do things on Nav Close
+            $(".wrapper").removeClass("throb");
+        }
+    }
+}
+
+
